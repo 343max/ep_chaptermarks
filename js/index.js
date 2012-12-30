375c656148901a471dea42560e12ee62f9eafb04
Max Winde <max.winde@gmail.com>
2012-12-30 14:58:32 +0100

    inserted some button into the toolbar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
-    console.log('-------------------------------------------------------');
-    args.content = require('ep_etherpad-lite/node/eejs/').require("ep_chaptermarks/templates/audioPlayer.ejs") + args.content;
+var eejs = require('ep_etherpad-lite/node/eejs/');
+
+exports.eejsBlock_editbarMenuRight = function(hook_name, args, cb) {
+    console.log('------------------------------------------------------- right');
+    args.content = eejs.require('ep_chaptermarks/templates/audioPlayer.ejs') + args.content;
+}
+
+exports.eejsBlock_editbarMenuLeft = function(hook_name, args, cb) {
+    console.log('------------------------------------------------------- left');
+    args.content += eejs.require('ep_chaptermarks/templates/timestampButton.ejs');
 }
